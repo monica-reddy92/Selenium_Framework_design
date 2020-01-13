38f321c724b0bb269bb47b2a73d14ee2181670c8
monica-reddy92 <59096515+monica-reddy92@users.noreply.github.com>
2020-01-14 01:23:25 +0530

    Add files via upload

---
+++ Makemytripapp/LoginPageTest.java
@@ -0,0 +1,51 @@
+package com.makemytrip.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.makemytrip.qa.base.TestBase;
+import com.makemytrip.qa.pages.FlightsPage;
+import com.makemytrip.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase
+{
+	LoginPage loginPage;
+	FlightsPage flightsPage;
+	
+	public LoginPageTest(){
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp() throws InterruptedException{
+		initialization();
+		//Thread.sleep(5000);
+		loginPage = new LoginPage();	
+	}
+	
+	@Test(priority=1)
+	public void loginPageTitleTest(){
+		String title = loginPage.validateLoginPageTitle();
+		System.out.println(title);
+		Assert.assertEquals(title, "MakeMyTrip - #1 Travel Website 50% OFF on Hotels, Flights & Holiday");
+	}
+	
+/*	@Test(priority=2)
+	public void makeMytripLogoImageTest(){
+		boolean flag = loginPage.validateMakemyTripImage();
+		Assert.assertTrue(flag);
+	}
+	
+	@Test(priority=3)
+	public void loginTest(){
+		flightsPage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}*/
+	
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+}
